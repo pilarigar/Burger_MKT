b6d059c117640a55c9207f74aeffdf3e042d691d
Pilar Igartua <piligartua@gmail.com>
2022-06-20 20:44:28 -0300

    carrito de compras

--- JS/menu.js
+++ JS/menu.js
@@ -4,7 +4,7 @@ const productos = [
       "Nombre": "Classic burguer",
       "Descripcion": "Doble medallón de 120g de carne vacuna, cheddar x4, lechuga, tomate, cebolla morada, pepinillos, salsa Thousand Island",
       "Precio": 1150,
-      "Foto":"" ,
+      "Foto":"https://lh5.googleusercontent.com/p/AF1QipMeYiD23AXzl7rQKmUKLQq31KeMSBTOLRL5LVWo=w650-h441-k-no",
       "Categoria": "Comida"
   },
 
@@ -13,7 +13,7 @@ const productos = [
       "Nombre": "Cheese burger",
       "Descripcion": "Medallón de 120g de carne vacuna, cheddar x4",
       "Precio": 1100,
-      "Foto": "",
+      "Foto": "https://images.rappi.com.ar/products/65f6ed98-c40a-4b3e-b7ad-2349da370a9f-1604070925374.jpeg",
       "Categoria": "Comida"
   },
 
@@ -22,7 +22,7 @@ const productos = [
       "Nombre": "Crispy burger",
       "Descripcion": "Doble medallón de 120g de carne vacuna, cheddar x4, bacon, cebolla crispy, alioli",
       "Precio": 1200,
-      "Foto": "",
+      "Foto": "https://images.deliveryhero.io/image/pedidosya/products/26893560-e3ef4182-2e9e-422a-87ec-76178aefcd49.jpeg?quality=90&width=248",
       "Categoria": "Comida"
       
   },
@@ -32,7 +32,7 @@ const productos = [
       "Nombre": "Mushroom burger",
       "Descripcion": "Doble medallón de 120g de carne vacuna, queso emmental x4, hongos salteados, cebolla caramelizada, queso azul",
       "Precio": 1200,
-      "Foto": "",
+      "Foto": "https://images.rappi.com.ar/products/e44d7d61-7ecd-4057-93dc-a4e539e792e8-1604070727032.jpeg",
       "Categoria": "Comida"
   }, 
       
@@ -41,7 +41,7 @@ const productos = [
       "Nombre": "Mexican burguer",
       "Descripcion": "Doble medallón de 120g de carne vacuna, cheddar x4, nachos, guacamole, salsa de cheddar",
       "Precio": 1200,
-      "Foto": "",
+      "Foto": "https://images.deliveryhero.io/image/pedidosya/products/26893567-1ed1f6a6-b435-40cb-a549-d06603233c01.jpeg?quality=90&width=248",
       "Categoria": "Comida"
       
   },
@@ -51,7 +51,7 @@ const productos = [
       "Nombre": "Medallón extra",
       "Descripcion": "Medallón de 120g de carne vacuna",
       "Precio": 120,
-      "Foto": "",
+      "Foto": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQD_2xKYIcNMBNOOH9nxOiIE3T9ny3fz62bWQ&usqp=CAU",
       "Categoria": "Comida"
   },
 
@@ -60,16 +60,16 @@ const productos = [
       "Nombre": "Vegan burger",
       "Descripcion": "Medallón isnot, cheddar x4, cebolla caramelizada, hongos, mayonesa vegana",
       "Precio": 1200,
-      "Foto": "",
+      "Foto": "https://images.rappi.com.ar/products/2160117-1619275619488.png",
       "Categoria": "Comida"
   },
 
   {
       "Id": 8,
       "Nombre": "Papas chicas",
-      "Descripcion": "Porción de papas para uno",
+      "Descripcion": "Porción de papas",
       "Precio": 500,
-      "Foto": "",
+      "Foto": "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/12/la-racion-de-papas-fritas-que-debes-consumir-diario.jpg",
       "Categoria": "Papasfritas"
   },
 
@@ -78,7 +78,7 @@ const productos = [
       "Nombre": "Papas grandes",
       "Descripcion": "Porción de papas para compartir",
       "Precio": 800,
-      "Foto": "",
+      "Foto": "https://cdn2.cocinadelirante.com/sites/default/files/styles/gallerie/public/images/2018/12/la-racion-de-papas-fritas-que-debes-consumir-diario.jpg",
       "Categoria": "Papasfritas"
   },
 
@@ -87,7 +87,7 @@ const productos = [
       "Nombre": "Gaseosas",
       "Descripcion": "línea coca-cola",
       "Precio": 400,
-      "Foto": "",
+      "Foto": "https://www.parrillalosviejos.com.ar/138-large_default/gaseosa-linea-coca-cola-500-ml.jpg",
       "Categoria": "Bebidas"
   },
 
@@ -96,7 +96,7 @@ const productos = [
       "Nombre": "Agua mineral",
       "Descripcion": "línea coca-cola",
       "Precio": 350,
-      "Foto": "",
+      "Foto": "http://infokioscos.com.ar/wp-content/uploads22/Bonaqua-botella.png",
       "Categoria": "Bebidas"
   },
 
@@ -105,14 +105,45 @@ const productos = [
       "Nombre": "Limonada",
       "Descripcion": "Limonada casera, con menta, jengibre y azucar",
       "Precio": 400,
-      "Foto": "",
+      "Foto": "https://www.ifeelgood.com.ar/img/articulos/limonada_con_curcuma_imagen1.jpg",
       "Categoria": "Bebidas"
   }
 ]
 
+const productosLista = document.getElementById('productos')
+const productoscarrito = document.getElementById('carrito')
+
+const nuevoarray = productos.find(producto => producto.Id === 1)
+console.log(nuevoarray) 
+const renderCard = () => {
+    let productosPanelVista = ''
+    productos.forEach(producto => {
+        {
+            productosPanelVista += `<div class="col-12 mb-2 col-md-4 col-sm-4 ">
+                <div class="card">
+                    <div class="card-body">
+                        <img id="fotoProducto" src="${producto.Foto}" widht=200px height=200px  >
+                        <h5 id="tituloProducto">${producto.Nombre}</h5>
+                        <p id="descripcionProducto">${producto.Descripcion}</p>
+                        <p id="precioProducto">$${producto.Precio}</p>
+                        <button data-id="${producto.Id}" id="mybtn" name="btnComprar" class="btn btn-success">Comprar</button>
+                    </div>
+                </div>
+            </div>
+            `
+        }
+    });
+    document.getElementById('productos').innerHTML = productosPanelVista
+
+}
+renderCard() 
+
+
+/* 
+
 let carrito = []
 
-//Ejemplo con ciclo usando map y reduce
+
 const agregarProductoCarrito = (id) => {
   let productoEncontrado = productos.find(producto => producto.Id === id)
   let productoCarrito = carrito.find(producto => producto.Id === id)
@@ -156,4 +187,4 @@ while (nombre !== "salir") {
           "Precio Total: " + carrito.reduce((acc, { Precio }) => acc + Precio, 0))
       break
   }
-} 
+} */
